58463d3adf399e94bd5b3114888d70eb19949f04
Nwankwo Onyedika Benson <nwankwobenson29@gmail.com>
2024-11-09 15:46:05 +0100

    Written the data handler file

--- client/data.py
+++ client/data.py
@@ -3,16 +3,17 @@ import csv
 
 #Creating My own data object called person
 
-class Person():
+class Person:
     def __init__(self,first_name,last_name,reg_no,department):
         self.first_name = first_name
         self.last_name = last_name
         self.reg_no  = reg_no
         self.department = department
 
-    def __repr__(self): #Returning data just incase I need to log errors
-
+    def __repr__(self):
         print(f"First name:{ self.first_name } \n Last name: { self.last_name } \n Reg_No:{ self.reg_no } \n Department: { self.department }")
+
+    def person_data(self):
 
         return {
             'first_name':self.first_name,
@@ -23,7 +24,7 @@ class Person():
 
 
 #Get the home folder
-def home_folder():
+def get_home_folder():
     home_folder = os.path.expanduser("~")
     if not os.path.isdir(home_folder):
         home_folder = os.environ.get("HOME") or os.environ.get("EXTERNAL_STORAGE") or "/storage/emulated/0"
@@ -31,5 +32,23 @@ def home_folder():
     return home_folder
 
 #Handling data from csv file
-data_folder = home_folder()
-data_file = os.path.join(data_folder)
+class File_Handler:
+    def __init__(self):
+        os.chdir(get_home_folder)
+        self.file_name = "student_data.csv"
+    def read_file(self):
+        if os.path.isfile(self.file_name):
+            with open(self.file_name,"r") as data:
+                reader = csv.DictReader(data)
+                row = next(reader,None)
+                return row
+        else:
+            with open(self.file_name,"r") as test:
+                pass
+
+    def write_file(self,content):
+        if os.path.isfile(self.file_name):
+            with open(self.file_name,"w") as data:
+                writer = csv.DictWriter(data,content.keys())
+                writer.writeheader()
+                writer.writerow(content)
